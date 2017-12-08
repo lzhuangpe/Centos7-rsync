1908b3dcf7fe5ea44e95e31c50924634cce7b529
huangpe <ahuangpe@163.com>
2017-12-08 14:03:47 +0800

    2017-12-08 14:03

--- server.sh
+++ server.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-Version=rsync-3.1.2
 User=backup
 Passwd=backup
 BackPath=/backup
 
-yum install -y gcc-c++
-wget https://download.samba.org/pub/rsync/rsync-3.1.2.tar.gz
-tar -zxf $Version.tar.gz && cd $Version
-./configure
-make && make install
+yum install -y rsync
 
 echo "$User:$Passwd" >>/etc/rsyncd.passwd
 chmod 600 /etc/rsyncd.passwd
@@ -37,10 +32,10 @@ auth users = backup                     #同步的虚拟用户名
 secrets file = /etc/rsyncd.passwd       #虚拟用户密码文件
 hosts allow = 192.168.1.0/24            #允许访问的IP
 hosts deny = *                          #拒绝所有IP访问
-read only =  no                         #设置可写
-use chroot = no                         #不允许切换目录
-list = no                               #不允许显示列表
-ignore errors                           #忽略错误
+read only =  false                      #设置可写
+use chroot = false                      #不允许切换目录
+list = false                            #不允许显示列表
+ignore = errors                         #忽略错误
 EOF
 
 echo "/usr/bin/rsync --daemon" >> /etc/rc.d/rc.local
