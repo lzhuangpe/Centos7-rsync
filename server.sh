a8f1c0e3fc18a3d0b878b6426058740edd648bfe
huangpe <ahuangpe@163.com>
2017-12-08 13:48:30 +0800

    2017-12-8 13:48

--- server.sh
+++ server.sh
@@ -15,7 +15,6 @@ echo "$User:$Passwd" >>/etc/rsyncd.passwd
 chmod 600 /etc/rsyncd.passwd
 
 mkdir -p /$BackPath
-chown -R nobody:nobody $BackPath
 
 cat > /etc/rsyncd.conf << EOF
 # /etc/rsyncd: configuration file for rsync daemon mode
@@ -24,24 +23,27 @@ cat > /etc/rsyncd.conf << EOF
 
 # configuration example:
 
-uid = nobody                        #运行rsync的用户
-gid = nobody                        #运行rsync的用户
-use chroot = no                     #不允许切换目录
-max connections = 5                 #最大连接数
-timeout = 600                       #超时时间
+uid = root
+gid = root
+max connections = 5                     #最大连接数
+timeout = 600                           #超时时间
 pid file = /var/run/rsyncd.pid
 lock file = /var/run/rsync.lock
 log file = /var/log/rsync.log
-ignore errors                       #忽略错误
-read only = false                   #设置可写
-list = false                        #不允许显示列表
-hosts allow = 172.16.100.0/24       #允许访问的IP
-hosts deny = *                      #拒绝所有IP访问
-auth users = rsync_backup           #同步的虚拟用户名
-secrets file = /etc/rsync.passwd    #虚拟用户密码
 
 [backup]
 path = /backup
+auth users = backup                     #同步的虚拟用户名
+secrets file = /etc/rsyncd.passwd       #虚拟用户密码文件
+hosts allow = 192.168.1.0/24            #允许访问的IP
+hosts deny = *                          #拒绝所有IP访问
+read only =  no                         #设置可写
+use chroot = no                         #不允许切换目录
+list = no                               #不允许显示列表
+ignore errors                           #忽略错误
 EOF
 
-systemctl enable rsyncd && systemctl start rsyncd
+echo "/usr/bin/rsync --daemon" > /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+/usr/bin/rsync --daemon
+
