08dc6a03d552afbcef4415754abf0844ad77d119
huangpe <ahuangpe@163.com>
2017-12-09 16:34:52 +0800

    2017-12-09 16:34

--- client.sh
+++ client.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-Version=rsync-3.1.2
 Passwd=backup
 
-yum install -y gcc-c++
-wget https://download.samba.org/pub/rsync/rsync-3.1.2.tar.gz
-tar -zxf $Version.tar.gz && cd $Version
-./configure
-make && make install
-
+yum install -y rsync
 echo "$Passwd" >>/etc/rsyncd.passwd
 chmod 600 /etc/rsyncd.passwd
